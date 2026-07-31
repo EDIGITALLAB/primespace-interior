5f21ba9e010bdb24d0557b254cb9c6455aa6e19c
EDIGITALLAB <helloelab123@gmail.com>
2026-07-31 20:37:59 +0530

    project_modify

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { ServiceDetails } from './pages/service-details/service-details';
 import { CategoriesPage } from './pages/categories-page/categories-page';
 import { ContactPage } from './pages/contact-page/contact-page';
 import { LocationsPage } from './pages/locations-page/locations-page';
+import { ProjectsPage } from './pages/projects-page/projects-page';
 
 export const routes: Routes = [
   { path: '', component: Landing },
   { path: 'categories', component: CategoriesPage },
+  { path: 'projects', component: ProjectsPage },
   { path: 'contact', component: ContactPage },
   { path: 'locations', component: LocationsPage },
   { path: 'locations/:city', component: LocationsPage },
